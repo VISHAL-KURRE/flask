4c61bb5268d40794881240e191f403f3e2684f66
vishal <vishalkurre28@gmail.com>
2023-01-31 19:33:06 -0800

    first

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
 
 from flask import Flask, render_template
 from app import app
+from . import templates
+
 
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('templates/index.html')
